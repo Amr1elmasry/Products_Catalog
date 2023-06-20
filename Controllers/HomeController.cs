37129a6508c80ff547926f092c9bb8b58ef3b6ec
Amr Elmasry <amr1elmasry@outlook.com>
2023-06-20 19:19:40 +0300

    add Edit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Mapster;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Product_Catalog.Interfaces;
 using Product_Catalog.Models;
 using Product_Catalog.ViewModels;
@@ -10,10 +12,14 @@ namespace Product_Catalog.Controllers
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ProductsCatalogDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(IProductService productService)
+        public HomeController(IProductService productService, ProductsCatalogDbContext context, UserManager<ApplicationUser> userManager)
         {
             _productService = productService;
+            _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
@@ -21,6 +27,19 @@ namespace Product_Catalog.Controllers
             var products = await _productService.GetAll();
             var output = products.Adapt<IEnumerable<ProductsViewModel>>();
             return View(output);
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var product = await _productService.FindById(id!.Value);
+            if (product == null)
+            {
+                return BadRequest("No Product Found With this id!");
+            }
+            var admins = await _userManager.GetUsersInRoleAsync("Admin");
+            ViewData["admins"] = new SelectList(admins , "Id" , "FirstName");
+            return View(product.Adapt<ProductsViewModel>());
         }
     }
 }
